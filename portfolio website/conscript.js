b5ed05acaf3c1677ca1793c6ee8dcc6eea389fe8
Sonam kumari <158497116+sonam-0612@users.noreply.github.com>
2024-03-30 12:02:25 +0530

    Add files via upload

---
+++ portfolio website/conscript.js
@@ -0,0 +1,3 @@
+//document.getElementById('contactForm').addEventListener('submit', function (event) {
+ //   event.preventDefault();
+// Add your code here to handle form submission, like sending the data to a server or displaying a success
